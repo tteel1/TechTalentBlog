b6f029a1257b070a547f62906ecb657d9190b042
Terrence <teelt@ymail.com>
2022-11-21 14:22:06 -0500

    first commit

---
+++ src/main/java/com/techtalentsouth/TechTalentBlog/BlogPostController.java
@@ -0,0 +1,54 @@
+package com.techtalentsouth.TechTalentBlog;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BlogPostController {
+	
+	
+	 @Autowired
+	    private BlogPostRepository blogPostRepository;
+	 private static List<BlogPost> posts = new ArrayList<>();
+	
+	@GetMapping(value="/")
+	public String index(BlogPost blogPost, Model model) {
+		model.addAttribute("posts", posts);
+		return "blogpost/index";
+	    }
+	
+	
+	 private BlogPost blogPost;
+	 
+	 @GetMapping(value = "/blogposts/new")
+	 public String newBlog (BlogPost blogPost) {
+	     return "blogpost/new";
+	 }
+	    
+	    @PostMapping(value = "blogposts")
+	    public String addNewBlogPost(BlogPost blogPost, Model model) {
+		blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+		posts.add(blogPost);
+		model.addAttribute("title", blogPost.getTitle());
+		model.addAttribute("author", blogPost.getAuthor());
+		model.addAttribute("blogEntry", blogPost.getBlogEntry());
+		return "blogpost/result";
+	    }
+	    
+	    @RequestMapping(value = "/blogposts/{id}", method = RequestMethod.DELETE)
+	    public String deletePostWithId(@PathVariable Long id, BlogPost blogPost) {
+
+	        blogPostRepository.deleteById(id);
+	        return "blogpost/index";
+
+	    }
+
+}
---
+++ src/main/java/com/techtalentsouth/TechTalentBlog/BlogPostRepository.java
@@ -0,0 +1,8 @@
+package com.techtalentsouth.TechTalentBlog;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface BlogPostRepository extends CrudRepository <BlogPost, Long>{
+
+}
